3d47e761d00c68b02e666a6624616e43ac1ab4a9
Awesummer08 <Awesummer08@github.com>
2022-07-20 09:04:08 +0900

    init commit

---
+++ src/main/java/com/springboot/studynakyeong/StudyNakyeongApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studynakyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNakyeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyNakyeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studynakyeong/StudyNakyeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studynakyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyNakyeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
